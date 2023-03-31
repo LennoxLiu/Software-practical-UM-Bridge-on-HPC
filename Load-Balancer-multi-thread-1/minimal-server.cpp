273155102f81b5784ea78daac6065ee61b03c6d1
Elaphurus <105095085+LennoxLiu@users.noreply.github.com>
2023-03-31 12:17:17 +0200

    Hosting server at 0.0.0.0

--- Load-Balancer-multi-thread-1/minimal-server.cpp
+++ Load-Balancer-multi-thread-1/minimal-server.cpp
@@ -94,7 +94,10 @@ int main(int argc, char *argv[])
 
     // Set up and serve model
     ExampleModel model(test_delay);
-    std::string hostname;
+    
+    
+    std::string hostname = "0.0.0.0";
+    /*
     if (argc == 2)
     {
         hostname = argv[1];
@@ -106,6 +109,7 @@ int main(int argc, char *argv[])
         if (!hostname.empty())
             hostname.pop_back();
     }
+    */
     std::cout << "Hosting server at : "
               << "http://" << hostname << ":" << port << std::endl;
     umbridge::serveModels({&model}, hostname, port); // start server at the hostname
